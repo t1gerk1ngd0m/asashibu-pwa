f279d82301356cd0bdfc7d8a407cd1bde92b799d
Taiga Miwa <taiga.miwa888@gmail.com>
2020-05-05 22:23:01 +0900

    vue修正

--- pages/restaurants/new.vue
+++ pages/restaurants/new.vue
@@ -30,13 +30,25 @@
         label="店舗画像"
         accept="image/*"
         prepend-icon="mdi-camera"
-        @change="setImage"
       ></v-file-input>
       <!-- <v-img
         :src="image"
         aspect-ratio="1.7778"
         height="100%"
       ></v-img> -->
+      <!-- <v-btn
+        flat
+        icon
+        @click="pickImage"
+      >
+        <v-icon class="primary--text">add_a_photo</v-icon>
+      </v-btn>
+      <input
+        type="file"
+        style="display: none;"
+        ref="image"
+        v-on:change="onFileChange" mulitple="multiple"
+      > -->
       <v-menu
         ref="menu"
         :close-on-content-click="false"
@@ -103,7 +115,6 @@ export default {
     externalLink: '',
     inputImages: [],
     uploadImage: {},
-    image: '',
     uriRules: [
       value => !!value || '食べログURLは必須です',
       value => /^https:\/\/tabelog.com\/(.*)/.test(value) || '食べログURLを記入してください',
@@ -127,23 +138,20 @@ export default {
   methods: {
     async submit () {
       try {
-        let payload = {
-          restaurant: {
-            external_link: this.externalLink,
-            opening_hours: this.openingHours
-          }
-        }
-        let res = await this.$axios.post(`/api/v1/restaurants`, payload)
+        let headers = { 'Content-Type': 'application/json' }
+        let formData = new FormData()
+        formData.append("restaurant[name]", this.name)
+        formData.append("restaurant[external_link]", this.externalLink)
+        formData.append("restaurant[opening_hours]", this.openingHours)
+        formData.append("restaurant[closing_hours]", this.closingHours)
+        formData.append("restaurant[nearest_station]", this.station)
+        formData.append("restaurant[image]", this.uploadImage)
+        let res = await this.$axios.post(`/api/v1/restaurants`, formData, headers)
+        console.log(res)
       } catch(e) {
         console.log(e)
       }
     },
-
-    setImage () {
-      console.log(this.uploadImage)
-      this.image = window.URL.createObjectURL(this.uploadImage)
-      console.log(this.image)
-    }
   }
 }
 </script>
